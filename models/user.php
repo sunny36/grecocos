ff62f9ffeb4ff1ef420806ecc472b8f81b977ca5
Somchok Sakjiraphong <s@sunny.in.th>
2010-03-19 04:23:14 +0700

    validations for user

--- models/user.php
+++ models/user.php
@@ -2,5 +2,28 @@
 class User extends AppModel {
 	var $name = 'User';
 	var $displayField = 'username';
+	
+	var $validate = array(
+  	  'username' => array(
+  	    'rule' => array('minLength', 1),
+  	    'require' => true,
+  	    'allowEmpty' => false,
+  	    'message' => 'Please enter a user name'
+  	    ),
+	    
+      'password' => array(
+        'rule' => array('minLength', 1),
+        'require' => true, 
+        'allowEmpty' => false,
+        'message' => 'Please enter a password'
+        ),
+      
+	    'email' => array(
+	      'rule' => 'email'
+        'require' => true, 
+        'allowEmpty' => false,
+	      'message' => 'Please enter a valid email'
+	      )
+	  );
 }
 ?>
