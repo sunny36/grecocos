1c15432807ad0b947aabb9cc40102af5c881a96d
Somchok Sakjiraphong <s@sunny.in.th>
2010-03-19 01:46:35 +0700

    initial import

---
+++ config/database.php
@@ -0,0 +1,13 @@
+<?php
+class DATABASE_CONFIG {
+
+	var $default = array(
+		'driver' => 'mysql',
+		'persistent' => false,
+		'host' => '127.0.0.1',
+		'login' => 'root',
+		'password' => 'root',
+		'database' => 'grecocos',
+	);
+}
+?>
