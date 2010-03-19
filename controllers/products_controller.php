5b6ae422108a8fefc9de536a402e23b3489c03a3
Somchok Sakjiraphong <s@sunny.in.th>
2010-03-19 19:59:42 +0700

    products controller

---
+++ controllers/products_controller.php
@@ -0,0 +1,62 @@
+<?php
+class ProductsController extends AppController {
+
+	var $name = 'Products';
+
+	function index() {
+		$this->Product->recursive = 0;
+		$this->set('products', $this->paginate());
+	}
+
+	function view($id = null) {
+		if (!$id) {
+			$this->Session->setFlash(sprintf(__('Invalid %s', true), 'product'));
+			$this->redirect(array('action' => 'index'));
+		}
+		$this->set('product', $this->Product->read(null, $id));
+	}
+
+	function add() {
+		if (!empty($this->data)) {
+			$this->Product->create();
+			if ($this->Product->save($this->data)) {
+				$this->Session->setFlash(sprintf(__('The %s has been saved', true), 'product'));
+				$this->redirect(array('action' => 'index'));
+			} else {
+				$this->Session->setFlash(sprintf(__('The %s could not be saved. Please, try again.', true), 'product'));
+			}
+		}
+	}
+
+	function edit($id = null) {
+		if (!$id && empty($this->data)) {
+			$this->Session->setFlash(sprintf(__('Invalid %s', true), 'product'));
+			$this->redirect(array('action' => 'index'));
+		}
+		if (!empty($this->data)) {
+			if ($this->Product->save($this->data)) {
+				$this->Session->setFlash(sprintf(__('The %s has been saved', true), 'product'));
+				$this->redirect(array('action' => 'index'));
+			} else {
+				$this->Session->setFlash(sprintf(__('The %s could not be saved. Please, try again.', true), 'product'));
+			}
+		}
+		if (empty($this->data)) {
+			$this->data = $this->Product->read(null, $id);
+		}
+	}
+
+	function delete($id = null) {
+		if (!$id) {
+			$this->Session->setFlash(sprintf(__('Invalid id for %s', true), 'product'));
+			$this->redirect(array('action'=>'index'));
+		}
+		if ($this->Product->delete($id)) {
+			$this->Session->setFlash(sprintf(__('%s deleted', true), 'Product'));
+			$this->redirect(array('action'=>'index'));
+		}
+		$this->Session->setFlash(sprintf(__('%s was not deleted', true), 'Product'));
+		$this->redirect(array('action' => 'index'));
+	}
+}
+?>
---
+++ tests/cases/controllers/products_controller.test.php
@@ -0,0 +1,49 @@
+<?php
+
+Warning: date(): It is not safe to rely on the system's timezone settings. You are *required* to use the date.timezone setting or the date_default_timezone_set() function. In case you used any of those methods and you are still getting this warning, you most likely misspelled the timezone identifier. We selected 'Asia/Bangkok' for 'ICT/7.0/no DST' instead in /Users/somchok/Sites/cake/1.3.0-RC2/cake/console/templates/default/classes/test.ctp on line 22
+/* Products Test cases generated on: 2010-03-19 19:03:05 : 1269003545*/
+App::import('Controller', 'Products');
+
+class TestProductsController extends ProductsController {
+	var $autoRender = false;
+
+	function redirect($url, $status = null, $exit = true) {
+		$this->redirectUrl = $url;
+	}
+}
+
+class ProductsControllerTestCase extends CakeTestCase {
+	var $fixtures = array('app.product');
+
+	function startTest() {
+		$this->Products =& new TestProductsController();
+		$this->Products->constructClasses();
+	}
+
+	function endTest() {
+		unset($this->Products);
+		ClassRegistry::flush();
+	}
+
+	function testIndex() {
+
+	}
+
+	function testView() {
+
+	}
+
+	function testAdd() {
+
+	}
+
+	function testEdit() {
+
+	}
+
+	function testDelete() {
+
+	}
+
+}
+?>
