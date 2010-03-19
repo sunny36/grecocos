18b696dcbcee3b5fe8a3392ff2c678d5d5b9781b
Somchok Sakjiraphong <s@sunny.in.th>
2010-03-19 15:20:04 +0700

    test database connection

--- config/database.php
+++ config/database.php
@@ -9,5 +9,14 @@ class DATABASE_CONFIG {
 		'password' => 'root',
 		'database' => 'grecocos',
 	);
+	
+	var $test = array(
+		'driver' => 'mysql',
+		'persistent' => false,
+		'host' => '127.0.0.1',
+		'login' => 'root',
+		'password' => 'root',
+		'database' => 'grecocos',
+	);
 }
 ?>
