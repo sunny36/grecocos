e62eb70e7a6ccb1268b403ea244d9eeec131d6bd
Somchok Sakjiraphong <s@sunny.in.th>
2010-03-22 21:21:39 +0700

    authentication for userScope

--- app_controller.php
+++ app_controller.php
@@ -30,20 +30,35 @@
  * @package       cake
  * @subpackage    cake.app
  */
+
 class AppController extends Controller {
   var $components = array('Auth', 'Session');
 
   function beforeFilter(){
-    $this->Auth->userScope = array('User.status' => '010');
+    //$this->Auth->userScope = array('User.status' => '010');
     $this->Auth->fields = array('username' => 'email', 'password' => 'password');
-    $this->Auth->loginRedirect = array('controller' => 'home', 
+
+		if($this->action == 'login' && !empty($this->data['User']['email'])) {
+			$conditions = array('email' => $this->data['User']['email'], 'status' => '000');
+			if ($this->User->find('count', array('conditions' => $conditions))) {
+				$this->Session->setFlash("You cannot login yet. Your account needs to be confirmed by the co-ordinator", 
+																 'default', 
+																	array(), 
+																	'auth');
+				$this->redirect(array('action' => 'login'));
+			}
+		}
+		
+    $this->Auth->loginRedirect = array('controller' => 'carts', 
                                        'action' => 'index');
     $this->Auth->logoutRedirect = array('controller' => 'users', 
                                        'action' => 'login');                          
     $this->Auth->allow('signup');
     
     $this->set('loggedIn', $this->Auth->user('id'));   
-    $this->Auth->authorize = 'controller';                                      
+    $this->Auth->authorize = 'controller';         
+		$this->currentUser = $this->Auth->user();           
+		$this->Auth->loginError = "Invalid email or password";                  
   }
   
   function isAuthorized() { 
