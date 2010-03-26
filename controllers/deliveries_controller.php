9cbe7c26f6e531f72957fb045bb48af4ffce6e49
Somchok Sakjiraphong <s@sunny.in.th>
2010-03-27 02:55:13 +0700

    admin for deliveries

--- controllers/deliveries_controller.php
+++ controllers/deliveries_controller.php
@@ -3,61 +3,6 @@ class DeliveriesController extends AppController {
 
 	var $name = 'Deliveries';
 
-	function index() {
-		$this->Delivery->recursive = 0;
-		$this->set('deliveries', $this->paginate());
-	}
-
-	function view($id = null) {
-		if (!$id) {
-			$this->Session->setFlash(sprintf(__('Invalid %s', true), 'delivery'));
-			$this->redirect(array('action' => 'index'));
-		}
-		$this->set('delivery', $this->Delivery->read(null, $id));
-	}
-
-	function add() {
-		if (!empty($this->data)) {
-			$this->Delivery->create();
-			if ($this->Delivery->save($this->data)) {
-				$this->Session->setFlash(sprintf(__('The %s has been saved', true), 'delivery'));
-				$this->redirect(array('action' => 'index'));
-			} else {
-				$this->Session->setFlash(sprintf(__('The %s could not be saved. Please, try again.', true), 'delivery'));
-			}
-		}
-	}
-
-	function edit($id = null) {
-		if (!$id && empty($this->data)) {
-			$this->Session->setFlash(sprintf(__('Invalid %s', true), 'delivery'));
-			$this->redirect(array('action' => 'index'));
-		}
-		if (!empty($this->data)) {
-			if ($this->Delivery->save($this->data)) {
-				$this->Session->setFlash(sprintf(__('The %s has been saved', true), 'delivery'));
-				$this->redirect(array('action' => 'index'));
-			} else {
-				$this->Session->setFlash(sprintf(__('The %s could not be saved. Please, try again.', true), 'delivery'));
-			}
-		}
-		if (empty($this->data)) {
-			$this->data = $this->Delivery->read(null, $id);
-		}
-	}
-
-	function delete($id = null) {
-		if (!$id) {
-			$this->Session->setFlash(sprintf(__('Invalid id for %s', true), 'delivery'));
-			$this->redirect(array('action'=>'index'));
-		}
-		if ($this->Delivery->delete($id)) {
-			$this->Session->setFlash(sprintf(__('%s deleted', true), 'Delivery'));
-			$this->redirect(array('action'=>'index'));
-		}
-		$this->Session->setFlash(sprintf(__('%s was not deleted', true), 'Delivery'));
-		$this->redirect(array('action' => 'index'));
-	}
 	function admin_index() {
 		$this->Delivery->recursive = 0;
 		$this->set('deliveries', $this->paginate());
