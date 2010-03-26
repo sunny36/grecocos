5305db04de6fd5f1588e24e2d26bab09c86adffb
Somchok Sakjiraphong <s@sunny.in.th>
2010-03-27 03:55:59 +0700

    dynamic database configuration

--- config/database.php
+++ config/database.php
@@ -1,7 +1,7 @@
 <?php
 class DATABASE_CONFIG {
 
-	var $default = array(
+	var $local = array(
 		'driver' => 'mysql',
 		'persistent' => false,
 		'host' => '127.0.0.1',
@@ -18,5 +18,31 @@ class DATABASE_CONFIG {
 		'password' => 'root',
 		'database' => 'grecocos',
 	);
+	
+	var $production = array(
+		'driver' => 'mysql',
+		'persistent' => false,
+		'host' => 'sql313.ispace.in.th',
+		'login' => 'ispac_4967521',
+		'password' => 'jaewjing',
+		'database' => 'ispac_4967521_grecocos',
+	);
+	
+	function __construct() {
+	  $host_r = explode('.', $_SERVER['SERVER_NAME']);
+  	if(count($host_r)>2) while(count($host_r)>2)array_shift($host_r);
+  	$mainhost = implode('.', $host_r);
+  		switch(strtolower($mainhost)) {
+  			case 'localhost':
+  				$this->default = $this->local;
+  				break;
+  			case 'grecocos.co.cc':
+  				$this->default = $this->production;
+  				break;
+  			default:
+  				$this->default = $this->local;
+  		}
+  	}
+	
 }
 ?>
