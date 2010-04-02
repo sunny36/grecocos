71224116c0f54e45eadf3edf4c01a0038b08b071
Somchok Sakjiraphong <s@sunny.in.th>
2010-04-03 02:58:58 +0700

    RequestHandler component

--- app_controller.php
+++ app_controller.php
@@ -1,6 +1,6 @@
 <?php
 class AppController extends Controller {
-  var $components = array('Auth', 'Session');
+  var $components = array('Auth', 'Session', 'RequestHandler');
 
   function beforeFilter(){
     $this->Auth->fields = array('username' => 'email', 
