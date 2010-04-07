603deb0cdca8e5bc97d9850bab6b6538e54bbf81
Somchok Sakjiraphong <s@sunny.in.th>
2010-04-07 22:38:02 +0700

    routing for supplier

--- config/core.php
+++ config/core.php
@@ -94,7 +94,9 @@
  *	`admin_index()` and `/admin/controller/index`
  *	`manager_index()` and `/manager/controller/index`
  */
-	Configure::write('Routing.prefixes', array('admin'));
+  
+	Configure::write('Routing.prefixes', array('admin', 'supplier'));
+	
 
 /**
  * Turn off all caching application-wide.
