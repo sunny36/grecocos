3ed9a516cb5cb80e785dc6c6c0fbf20dd57eda38
Somchok Sakjiraphong <s@sunny.in.th>
2010-05-08 13:34:45 +0700

    Removed unused routes. Added route for dashboard.

--- config/routes.php
+++ config/routes.php
@@ -12,9 +12,6 @@
 	Router::connect('/pages/*', array('controller' => 'pages', 'action' => 'display'));
 	
 	Router::connect('/login', array('controller' => 'users', 'action' => 'login'));  
-  // Router::connect('/admin/users/login', array('controller' => 'users', 'action' => 'login'));
-  // Router::connect('/admin/users/logout', array('controller' => 'users', 'action' => 'logout'));
-  Router::connect('/admin', array('controller' => 'dashboard', 'action' => 'index', 'admin' => true));
-  Router::connect('/admin/coordinator/orders', array('controller' => 'orders', 'action' => 'index', 'prefix' => 'admin'));
-  Router::connect('/admin/supplier/orders', array('controller' => 'orders', 'action' => 'index', 'prefix' => 'supplier'));
-?>
+  Router::connect('/admin', array('controller' => 'dashboard', 'action' => 'index', 
+                                  'admin' => true));
+  ?>
