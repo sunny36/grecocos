35617a607b76ed999d069847aecb60e000c46cef
Somchok Sakjiraphong <s@sunny.in.th>
2010-05-10 22:40:36 +0700

    Re-indent file and add supplier index to categories controller.

--- controllers/categories_controller.php
+++ controllers/categories_controller.php
@@ -1,117 +1,120 @@
 <?php
 class CategoriesController extends AppController {
 
-	var $name = 'Categories';
+  var $name = 'Categories';
+  var $helpers = array('Html', 'Form', 'Javascript');
 
-	function index() {
-		$this->Category->recursive = 0;
-		$this->set('categories', $this->paginate());
-	}
+  function supplier_index() {
+    $this->layout = "supplier/index";
+    $this->Category->recursive = 0;
+    $this->set('categories', $this->paginate());
+    $this->render('/categories/admin_index');
+  }
 
-	function view($id = null) {
-		if (!$id) {
-			$this->Session->setFlash(sprintf(__('Invalid %s', true), 'category'));
-			$this->redirect(array('action' => 'index'));
-		}
-		$this->set('category', $this->Category->read(null, $id));
-	}
+  function view($id = null) {
+    if (!$id) {
+      $this->Session->setFlash(sprintf(__('Invalid %s', true), 'category'));
+      $this->redirect(array('action' => 'index'));
+    }
+    $this->set('category', $this->Category->read(null, $id));
+  }
 
-	function add() {
-		if (!empty($this->data)) {
-			$this->Category->create();
-			if ($this->Category->save($this->data)) {
-				$this->Session->setFlash(sprintf(__('The %s has been saved', true), 'category'));
-				$this->redirect(array('action' => 'index'));
-			} else {
-				$this->Session->setFlash(sprintf(__('The %s could not be saved. Please, try again.', true), 'category'));
-			}
-		}
-	}
+  function add() {
+    if (!empty($this->data)) {
+      $this->Category->create();
+      if ($this->Category->save($this->data)) {
+        $this->Session->setFlash(sprintf(__('The %s has been saved', true), 'category'));
+        $this->redirect(array('action' => 'index'));
+      } else {
+        $this->Session->setFlash(sprintf(__('The %s could not be saved. Please, try again.', true), 'category'));
+      }
+    }
+  }
 
-	function edit($id = null) {
-		if (!$id && empty($this->data)) {
-			$this->Session->setFlash(sprintf(__('Invalid %s', true), 'category'));
-			$this->redirect(array('action' => 'index'));
-		}
-		if (!empty($this->data)) {
-			if ($this->Category->save($this->data)) {
-				$this->Session->setFlash(sprintf(__('The %s has been saved', true), 'category'));
-				$this->redirect(array('action' => 'index'));
-			} else {
-				$this->Session->setFlash(sprintf(__('The %s could not be saved. Please, try again.', true), 'category'));
-			}
-		}
-		if (empty($this->data)) {
-			$this->data = $this->Category->read(null, $id);
-		}
-	}
+  function edit($id = null) {
+    if (!$id && empty($this->data)) {
+      $this->Session->setFlash(sprintf(__('Invalid %s', true), 'category'));
+      $this->redirect(array('action' => 'index'));
+    }
+    if (!empty($this->data)) {
+      if ($this->Category->save($this->data)) {
+        $this->Session->setFlash(sprintf(__('The %s has been saved', true), 'category'));
+        $this->redirect(array('action' => 'index'));
+      } else {
+        $this->Session->setFlash(sprintf(__('The %s could not be saved. Please, try again.', true), 'category'));
+      }
+    }
+    if (empty($this->data)) {
+      $this->data = $this->Category->read(null, $id);
+    }
+  }
 
-	function delete($id = null) {
-		if (!$id) {
-			$this->Session->setFlash(sprintf(__('Invalid id for %s', true), 'category'));
-			$this->redirect(array('action'=>'index'));
-		}
-		if ($this->Category->delete($id)) {
-			$this->Session->setFlash(sprintf(__('%s deleted', true), 'Category'));
-			$this->redirect(array('action'=>'index'));
-		}
-		$this->Session->setFlash(sprintf(__('%s was not deleted', true), 'Category'));
-		$this->redirect(array('action' => 'index'));
-	}
-	function admin_index() {
-		$this->Category->recursive = 0;
-		$this->set('categories', $this->paginate());
-	}
+  function delete($id = null) {
+    if (!$id) {
+      $this->Session->setFlash(sprintf(__('Invalid id for %s', true), 'category'));
+      $this->redirect(array('action'=>'index'));
+    }
+    if ($this->Category->delete($id)) {
+      $this->Session->setFlash(sprintf(__('%s deleted', true), 'Category'));
+      $this->redirect(array('action'=>'index'));
+    }
+    $this->Session->setFlash(sprintf(__('%s was not deleted', true), 'Category'));
+    $this->redirect(array('action' => 'index'));
+  }
+  function admin_index() {
+    $this->Category->recursive = 0;
+    $this->set('categories', $this->paginate());
+  }
 
-	function admin_view($id = null) {
-		if (!$id) {
-			$this->Session->setFlash(sprintf(__('Invalid %s', true), 'category'));
-			$this->redirect(array('action' => 'index'));
-		}
-		$this->set('category', $this->Category->read(null, $id));
-	}
+  function admin_view($id = null) {
+    if (!$id) {
+      $this->Session->setFlash(sprintf(__('Invalid %s', true), 'category'));
+      $this->redirect(array('action' => 'index'));
+    }
+    $this->set('category', $this->Category->read(null, $id));
+  }
 
-	function admin_add() {
-		if (!empty($this->data)) {
-			$this->Category->create();
-			if ($this->Category->save($this->data)) {
-				$this->Session->setFlash(sprintf(__('The %s has been saved', true), 'category'));
-				$this->redirect(array('action' => 'index'));
-			} else {
-				$this->Session->setFlash(sprintf(__('The %s could not be saved. Please, try again.', true), 'category'));
-			}
-		}
-	}
+  function admin_add() {
+    if (!empty($this->data)) {
+      $this->Category->create();
+      if ($this->Category->save($this->data)) {
+        $this->Session->setFlash(sprintf(__('The %s has been saved', true), 'category'));
+        $this->redirect(array('action' => 'index'));
+      } else {
+        $this->Session->setFlash(sprintf(__('The %s could not be saved. Please, try again.', true), 'category'));
+      }
+    }
+  }
 
-	function admin_edit($id = null) {
-		if (!$id && empty($this->data)) {
-			$this->Session->setFlash(sprintf(__('Invalid %s', true), 'category'));
-			$this->redirect(array('action' => 'index'));
-		}
-		if (!empty($this->data)) {
-			if ($this->Category->save($this->data)) {
-				$this->Session->setFlash(sprintf(__('The %s has been saved', true), 'category'));
-				$this->redirect(array('action' => 'index'));
-			} else {
-				$this->Session->setFlash(sprintf(__('The %s could not be saved. Please, try again.', true), 'category'));
-			}
-		}
-		if (empty($this->data)) {
-			$this->data = $this->Category->read(null, $id);
-		}
-	}
+  function admin_edit($id = null) {
+    if (!$id && empty($this->data)) {
+      $this->Session->setFlash(sprintf(__('Invalid %s', true), 'category'));
+      $this->redirect(array('action' => 'index'));
+    }
+    if (!empty($this->data)) {
+      if ($this->Category->save($this->data)) {
+        $this->Session->setFlash(sprintf(__('The %s has been saved', true), 'category'));
+        $this->redirect(array('action' => 'index'));
+      } else {
+        $this->Session->setFlash(sprintf(__('The %s could not be saved. Please, try again.', true), 'category'));
+      }
+    }
+    if (empty($this->data)) {
+      $this->data = $this->Category->read(null, $id);
+    }
+  }
 
-	function admin_delete($id = null) {
-		if (!$id) {
-			$this->Session->setFlash(sprintf(__('Invalid id for %s', true), 'category'));
-			$this->redirect(array('action'=>'index'));
-		}
-		if ($this->Category->delete($id)) {
-			$this->Session->setFlash(sprintf(__('%s deleted', true), 'Category'));
-			$this->redirect(array('action'=>'index'));
-		}
-		$this->Session->setFlash(sprintf(__('%s was not deleted', true), 'Category'));
-		$this->redirect(array('action' => 'index'));
-	}
+  function admin_delete($id = null) {
+    if (!$id) {
+      $this->Session->setFlash(sprintf(__('Invalid id for %s', true), 'category'));
+      $this->redirect(array('action'=>'index'));
+    }
+    if ($this->Category->delete($id)) {
+      $this->Session->setFlash(sprintf(__('%s deleted', true), 'Category'));
+      $this->redirect(array('action'=>'index'));
+    }
+    $this->Session->setFlash(sprintf(__('%s was not deleted', true), 'Category'));
+    $this->redirect(array('action' => 'index'));
+  }
 }
 ?>
