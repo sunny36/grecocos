52c31d6359cdf3b003381f762e9b33f53ae183aa
Somchok Sakjiraphong <s@sunny.in.th>
2010-05-10 20:38:10 +0700

    Added new header for supplier with menu.

--- controllers/dashboard_controller.php
+++ controllers/dashboard_controller.php
@@ -1,21 +1,21 @@
 <?php
 class DashboardController extends AppController{
   var $uses = array('Product', 'Order', 'LineItem', 'Delivery', 'Category', 'Cart');
-	var $helpers = array('Html', 'Form', 'Javascript');
-	
-	function admin_index() { 
-	  $this->set('title_for_layout', 'Site Administration | Grecocos site admin');
+  var $helpers = array('Html', 'Form', 'Javascript');
+  
+  function admin_index() { 
+    $this->set('title_for_layout', 'Site Administration | Grecocos site admin');
     $this->layout = "admin_dashboard"; 
-	}
-	
-	function admin_coordinator() { 
-	  $this->set('title_for_layout', 'Grecocos | Co-ordinator');
+  }
+  
+  function admin_coordinator() { 
+    $this->set('title_for_layout', 'Grecocos | Co-ordinator');
     $this->layout = "coordinator/dashboard"; 
-	}
+  }
 
-	function admin_supplier() { 
-	  $this->set('title_for_layout', 'Grecocos | Supplier');
-    $this->layout = "admin_dashboard"; 
-	}
-	
+  function admin_supplier() { 
+    $this->set('title_for_layout', 'Grecocos | Supplier');
+    $this->layout = "supplier/dashboard"; 
+  }
+  
 }
