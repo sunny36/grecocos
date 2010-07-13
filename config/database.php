9232c6abdc71418dcda596590d81d69a5e00e54f
Somchok Sakjiraphong <s@sunny.in.th>
2010-07-13 23:31:28 +0700

    Update database configuration.

--- config/database.php
+++ config/database.php
@@ -1,7 +1,7 @@
 <?php
 class DATABASE_CONFIG {
 
-	var $local = array(
+	var $default = array(
 		'driver' => 'mysql',
 		'persistent' => false,
 		'host' => '127.0.0.1',
@@ -16,33 +16,8 @@ class DATABASE_CONFIG {
 		'host' => '127.0.0.1',
 		'login' => 'root',
 		'password' => 'root',
-		'database' => 'grecocos',
+		'database' => 'grecocos_test',
 	);
-	
-	var $production = array(
-		'driver' => 'mysql',
-		'persistent' => false,
-		'host' => 'sql313.ispace.in.th',
-		'login' => 'ispac_4967521',
-		'password' => 'jaewjing',
-		'database' => 'ispac_4967521_grecocos',
-	);
-	
-	function __construct() {
-	  $host_r = explode('.', $_SERVER['SERVER_NAME']);
-  	if(count($host_r)>2) while(count($host_r)>2)array_shift($host_r);
-  	$mainhost = implode('.', $host_r);
-  		switch(strtolower($mainhost)) {
-  			case 'localhost':
-  				$this->default = $this->local;
-  				break;
-  			case 'grecocos.co.cc':
-  				$this->default = $this->production;
-  				break;
-  			default:
-  				$this->default = $this->local;
-  		}
-  	}
 	
 }
 ?>
