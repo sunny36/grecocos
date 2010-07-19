3ab368e79d78e6c3107e89b87e23aa5dd9118122
Somchok Sakjiraphong <s@sunny.in.th>
2010-07-20 02:37:08 +0700

    Transaction report 2.

--- controllers/transactions_controller.php
+++ controllers/transactions_controller.php
@@ -15,7 +15,6 @@ class TransactionsController extends AppController {
       if (isset($this->params['url']['delivery_date'])) {
         $delivery_id = $this->params['url']['delivery_date'];
       }
-      
       if(!$sidx) $sidx =1;
       $count = 0;
       if (isset($delivery_id)) {
@@ -31,7 +30,6 @@ class TransactionsController extends AppController {
       if ($page > $total_pages) $page=$total_pages;
       $start = $limit*$page - $limit;
       $recursive = 2;
-      $this->log($delivery_id, 'activity');
       $cashIn = null; 
       $cashOut = null; 
       if (isset($delivery_id)) {
@@ -70,6 +68,77 @@ class TransactionsController extends AppController {
       $this->set('cash_in', $cashIn); 
       $this->set('cash_out', $cashOut); 
       $this->render('/elements/transactions', 'ajax');
+    }
+  }
+  
+  function administrator_cash_report2() {
+    $this->layout = "admin_index"; 
+    if($this->RequestHandler->isAjax()) {
+      $page = $this->params['url']['page']; 
+      $limit = $this->params['url']['rows']; 
+      $sidx = $this->params['url']['sidx']; 
+      $sord = $this->params['url']['sord']; 
+      $delivery_id = null; 
+      if (isset($this->params['url']['delivery_date'])) {
+        $delivery_id = $this->params['url']['delivery_date'];
+      }
+      if(!$sidx) $sidx =1;
+      $count = 0;
+      if (isset($delivery_id)) {
+        $count = $this->Transaction->getNumRowsForPaidTransactionByDeliveryDate($delivery_id); 
+      } else {
+        $count = $this->Transaction->getNumRowsForPaidTransaction(); 
+      }
+      if( $count >0 ) {
+        $total_pages = ceil($count/$limit);
+      } else {
+        $total_pages = 0;
+      }
+      if ($page > $total_pages) $page=$total_pages;
+      $start = $limit*$page - $limit;
+      $recursive = 2;
+      $cashIn = null; 
+      $dueToPay = null; 
+      if (isset($delivery_id)) {
+        $transactions = $this->Transaction->getPaidTransactionByDeliveryDate($recursive, $start, $limit, 
+          $delivery_id);
+        $cashIn = $this->Transaction->getCashIn2($delivery_id); 
+        $dueToPay = $this->Transaction->getDueToPay($delivery_id); 
+      } else {
+        $transactions = $this->Transaction->getPaidTransaction($recursive, $start, $limit);
+        $cashIn = $this->Transaction->getCashIn2(); 
+        $dueToPay = $this->Transaction->getDueToPay(); 
+      }
+      App::import( 'Helper', 'Time' );
+      $time = new TimeHelper;
+      foreach($transactions as &$transaction) {
+        $transaction['Order']['ordered_date'] = 
+          $time->format($format = 'm-d-Y', $transaction['Order']['ordered_date']);
+        $transaction['Transaction']['cash_in'] = "";
+        $transaction['Transaction']['due_to_pay'] = "";
+        if ($transaction['Transaction']['type'] == 'Cash Payment') {
+          $transaction['Transaction']['cash_in'] = $transaction['Order']['total'];
+          $transaction['Transaction']['due_to_pay'] = $transaction['Order']['total2'];
+          $transaction['Transaction']['type'] = "Order" . " #" . $transaction['Transaction']['order_id'];
+        }
+        if ($transaction['Transaction']['type'] == "Refund") {
+          $amountRefundIn = -($transaction['Order']['total'] - $transaction['Order']['total_supplied']);
+          $transaction['Transaction']['cash_in'] = $amountRefundIn;
+          $amountRefundDue = -($transaction['Order']['total2'] - $transaction['Order']['total2_supplied']);
+          $transaction['Transaction']['due_to_pay'] = $amountRefundDue; 
+          $transaction['Transaction']['type'] = "Refund Order" . " #" . $transaction['Transaction']['order_id'];
+        }
+        if ($transaction['Transaction']['type'] == "Bank Transfer") {
+          $transaction['Transaction']['due_to_pay'] = -($transaction['Transaction']['bank_transfer_amount']);
+        }
+      }
+      $this->set('page',$page);
+      $this->set('total_pages',$total_pages);
+      $this->set('count',$count); 
+      $this->set('transactions', $transactions);   
+      $this->set('cash_in', $cashIn); 
+      $this->set('due_to_pay', $dueToPay); 
+      $this->render('/elements/transactions2', 'ajax');
     }
   }
 
---
+++ views/elements/transactions2.ctp
@@ -0,0 +1,47 @@
+<?php
+if ( stristr($_SERVER["HTTP_ACCEPT"],"application/xhtml+xml") ) {
+              header("Content-type: application/xhtml+xml;charset=utf-8"); 
+} else {
+          header("Content-type: text/xml;charset=utf-8");
+}
+echo "<?xml version='1.0' encoding='utf-8'?>";
+echo "<rows>";
+echo "<page>".$page."</page>";
+echo "<total>".$total_pages."</total>";
+echo "<records>".$count."</records>";
+echo "<userdata name=\"type\">" . "Totals" . "</userdata>";
+echo "<userdata name=\"cash_in\">" . $cash_in . "</userdata>";
+echo "<userdata name=\"due_to_pay\">" . $due_to_pay . "</userdata>";
+for($i=0;$i<sizeof($transactions);$i++){
+echo "<row id='".$transactions[$i]['Transaction']['id']."'>";            
+
+if ($transactions[$i]['Transaction']['type'] == "Bank Transfer") {
+  echo "<cell>". "Swift Co. Ltd." ."</cell>";
+} else {
+  echo "<cell>". $transactions[$i]['User']['name']."</cell>";
+}
+
+echo "<cell>". $transactions[$i]['Transaction']['type']."</cell>";
+
+App::import( 'Helper', 'Time' );
+if ($transactions[$i]['Transaction']['type'] == "Bank Transfer") {
+  echo "<cell>". $time->format($format = 'Y-m-d', $transactions[$i]['Transaction']['created']) ."</cell>";
+} else {
+  echo "<cell>". $transactions[$i]['Order']['ordered_date']."</cell>";
+}
+if ($transactions[$i]['Transaction']['type'] == "Bank Transfer") {
+  echo "<cell>". $time->format($format = 'Y-m-d', $transactions[$i]['Transaction']['from']) . " TO " . 
+       $time->format($format = 'Y-m-d', $transactions[$i]['Transaction']['to']) . "</cell>";
+       
+} else {
+  echo "<cell>". $transactions[$i]['Order']['Delivery']['date']."</cell>";  
+}
+echo "<cell>". $transactions[$i]['Transaction']['cash_in']."</cell>";
+echo "<cell>". $transactions[$i]['Transaction']['due_to_pay']."</cell>";
+echo "</row>";
+}
+echo "</rows>"; 
+
+
+?>
+
