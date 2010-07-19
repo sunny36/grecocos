d90f02214d834e7be3f7b6f852fcf90356b899bd
Somchok Sakjiraphong <s@sunny.in.th>
2010-07-19 16:46:20 +0700

    Display net cash in in transactions.

--- webroot/js/transactions/index.js
+++ webroot/js/transactions/index.js
@@ -6,11 +6,11 @@ $(document).ready(function(){
     url: window.location.pathname, 
     colNames: ['From/To', 'Description', 'Date', 'Batch', 'Cash In', 'Cash Out'], 
     colModel:[
-        {name:'user_name',index:'user_name', align:"left", search:false, width: 200},
-        {name:'type', index:'type', align:"left", search:false},
-        {name:'ordered_date', index:'type', align:"left", search:false},
-        {name:'delivery_date', index:'delivery_date',  align:"left", stype:'select', 
-         searchoptions:{value:"all:All"}, width:"180"},
+      {name:'user_name',index:'user_name', align:"left", search:false, width: 200},
+      {name:'type', index:'type', align:"left", search:false},
+      {name:'ordered_date', index:'type', align:"left", search:false},
+      {name:'delivery_date', index:'delivery_date',  align:"left", stype:'select', 
+        searchoptions:{value:"all:All"}, width:"180"},
         {name:'cash_in',index:'cash_in',align:"right", search:false},
         {name:'cash_out',index:'created',  align:"right", search:false}
     ],
@@ -21,19 +21,32 @@ $(document).ready(function(){
     footerrow: true,
     userDataOnFooter: true,
     rowNum: 20,
-   	rowList:[10,20,30],
-   	caption: "Transactions",
+    rowList:[10,20,30],
+    caption: "Transactions",
     pager: jQuery('#transactions_pager'),
+    gridComplete: function() {
+      $('.ui-jqgrid-sdiv').clone().appendTo('.ui-jqgrid-sdiv'); 
+      $($('table.ui-jqgrid-ftable')[1]).children().children().children()[1].innerHTML = "Net cash in for this batch"; 
+      var cashIn = parseInt($($('table.ui-jqgrid-ftable')[1]).children().children().children()[4].innerHTML, 10);
+      var cashOut = parseInt($($('table.ui-jqgrid-ftable')[1]).children().children().children()[5].innerHTML, 10);
+      var netCash = cashIn - cashOut; 
+      $($('table.ui-jqgrid-ftable')[1]).children().children().children()[4].innerHTML = netCash; 
+      $($('table.ui-jqgrid-ftable')[1]).children().children().children()[5].innerHTML = ""; 
+    }, 
+    beforeRequest: function() {
+      $($('.ui-jqgrid-sdiv')[1]).remove(); 
+    }
   }).navGrid('#transactions_pager',{edit:false,add:false,del:false});	;
 
   jQuery("#transactions").jqGrid('filterToolbar');
   $.get('/index.php/supplier/deliveries/getalljson', function(data) {
     var delivery_dates = eval('(' + data + ')');
     for(i = 0; i < delivery_dates.length; i++) {
-      console.log(delivery_dates[i]["Delivery"]["id"]);
-      console.log(delivery_dates[i]["Delivery"]["date"]);
-      $('#gs_delivery_date').append($("<option></option>").attr("value",delivery_dates[i]["Delivery"]["id"]).text(delivery_dates[i]["Delivery"]["date"]));
+      $('#gs_delivery_date').
+        append($("<option></option>").
+               attr("value",delivery_dates[i]["Delivery"]["id"]).
+                 text(delivery_dates[i]["Delivery"]["date"]));
     }
   });
-  
+
 });
