96fcb43ac6d3b235e95ff0790daf012b7ea3a5c7
Somchok Sakjiraphong <s@sunny.in.th>
2010-08-14 01:14:36 +0700

    Check whether emails has to be sent from configurations.

--- models/appengine_email.php
+++ models/appengine_email.php
@@ -3,23 +3,28 @@ class AppengineEmail extends AppModel {
   var $useTable = false; 
 
   function sendEmail($to, $subject, $body, $attachment = NULL) {
-    $request_url = 'http://grecocos.appspot.com/sendemail';
-    $post_params['to'] = $to;
-    $post_params['subject'] = $subject; 
-    $post_params['body'] = $body;
-    if ($attachment != NULL) {
-      $post_params['file_name'] = '@' . $attachment;
-    } else {
-      $post_params['file_name'] = NULL;
+    if (Configure::read('Grecocos.send_email') == "no") {
+      return; 
+    } 
+    if (Configure::read('Grecocos.send_email') == "yes") {
+      $request_url = 'http://grecocos.appspot.com/sendemail';
+      $post_params['to'] = $to;
+      $post_params['subject'] = $subject; 
+      $post_params['body'] = $body;
+      if ($attachment != NULL) {
+        $post_params['file_name'] = '@' . $attachment;
+      } else {
+        $post_params['file_name'] = NULL;
+      }
+      $post_params['submit'] = urlencode('Send');
+      $ch = curl_init();
+      curl_setopt($ch, CURLOPT_URL, $request_url);
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+      curl_setopt($ch, CURLOPT_POST, true);
+      curl_setopt($ch, CURLOPT_POSTFIELDS, $post_params);
+      $result = curl_exec($ch);
+      curl_close($ch);
     }
-    $post_params['submit'] = urlencode('Send');
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $request_url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_POST, true);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $post_params);
-    $result = curl_exec($ch);
-    curl_close($ch);
   }
 }
 ?>
