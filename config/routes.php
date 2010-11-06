15d586ebde01a8f38a57a7771367743d184e5c4d
Somchok Sakjiraphong <s@sunny.in.th>
2010-11-06 14:06:54 +0700

    Add routes for csv file.

--- config/routes.php
+++ config/routes.php
@@ -4,6 +4,7 @@
  * its action called 'display', and we pass a param to select the view file
  * to use (in this case, /app/views/pages/home.ctp)...
  */
+ 
 	Router::connect('/', array('controller' => 'users', 'action' => 'login'));
 
 /**
@@ -22,5 +23,5 @@
                                                    'supplier' => true));
   Router::connect('/coordinator/batch_reports', array('controller' => 'line_items', 'action' => 'index', 
                                                       'coordinator' => true));
-  
+  Router::parseExtensions('csv'); 
 ?>
