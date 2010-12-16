77cc48aec452b4e2c5eb5f2b3c61179430f81083
Somchok Sakjiraphong <s@sunny.in.th>
2010-12-17 03:58:57 +0700

    Organization has many delivery and configuration.

--- models/organization.php
+++ models/organization.php
@@ -1,7 +1,7 @@
 <?php
 class Organization extends AppModel {
 	var $name = 'Organization';
-  var $hasMany = 'User';
+  var $hasMany = array('User', 'Delivery', 'Configuration');
 
   function getOrganizationsList() {
     return $this->find('list', array('fields' => array('Organization.id', 'Organization.delivery_address')));
